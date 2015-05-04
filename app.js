3a2d8160bec0320e3515ed07b13c2b953d768a8e
SunChero <sunshero@msn.com>
2015-05-04 10:16:32 -0400

    start2

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
+/// <reference path="typings/node/node.d.ts"/>
+
+
 var express = require('express');
 
 var app = express();
 
-app.use('__dirname' + 'me');
+app.get('/', function(req, res){
+	
+	res.end('OK!');
+	
+});
+
+app.listen(process.env.PORT || 80);
+
+
+
 
 
 
